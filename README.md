38feec7845c2f35a584fc0bc0f39df641e01c8d5
geogiadim <gia1914g@gmail.com>
2021-11-10 01:01:16 +0200

    join and play commands completed + readme update

--- README.md
+++ README.md
@@ -3,67 +3,85 @@
 ### About 
 ___
 This is a discord music bot that plays music from YouTube in discord voice channels.
+- Follow the [installation guide](#installation-guide)
+- After install [requirements](#requirements)
+- Then [connect this bot with your Discord Application](#connect-this-bot-with-your-discord-application)
+- Finally, [run](#run) this bot!!!
 
 ### Installation Guide 
 ___
 
-- Clone this repository into a directory.
-- Open your terminal (for linux) or cmd (for windows) inside the new directory.
-- Install a python virtual environment inside your new directory:
-  ```
-  $ pip install virtualenv
-  $ virtualenv -p python3 venv
-  ```
-  For linux:
-  ```  
-  $ virtualenv venv 
-  ```
-  For windows:
-  ```
-  $ virtualenv -p python3 venv
-  ```
-- Activate the installed venv:
-  <br><br>For Windows:
-  ```
-  $ venv\Scripts\activate
-  ```
-  For Linux:
-  ```
-  $ . venv/bin/activate
-  ```
+1. Fork this project.
+2. Clone this repository into a directory on your local machine.
+3. Install a python virtual environment inside your new directory
+    ```
+    $ pip install virtualenv
+    $ virtualenv -p python3 venv
+    ```
+    For Linux or Mac OS
+    ```  
+    $ virtualenv venv 
+    ```
+    For Windows
+    ```
+    $ virtualenv -p python3 venv
+    ```
+   
+4. Activate the installed venv using below commands  
+    For Linux:
+    ```
+    $ . venv/bin/activate
+    ```
+    For Windows:
+    ```
+    $ venv\Scripts\activate
+    ```
   
 ### Requirements 
 ___
-In the project directory, install the packages using:
+In the project directory, install the required packages inside your activated ```venv``` using
 ```
-pip install -r requirements.txt
+$ pip install -r requirements.txt
+```
+
+### Connect this bot with your Discord Application
+___
+Create a Bot from the [Discord Developer Portal](https://discord.com/developers/docs/intro), generate a Bot Token and copy to your clipboard the Bot token.  
+Then create a ```.env``` file and paste the Token.
+```
+TOKEN = "Your token here" 
 ```
 ### Run
 ___
-In order to run this Bot run below command inside your activated venv:
+In order to run this Discord Bot run below command inside your activated ```venv```
 ```
-$ python main.py
+$ python app.py
 ```
 
 ### Bot Commands
 ___
 
-Join in a voice channel and play given YouTube url:
+Join in a voice channel 
 ```
-?play "youtube_url"
+?join
+```
+
+Play given YouTube url or query
+```
+?play "youtube_url or query" 
 ``` 
 
-Resume playing music:
+Resume playing music
 ```
 ?resume
 ``` 
 
-Pause playing music:
+Pause playing music
 ```
 ?pause
 ```
 
-Disconnect from the voice channel: 
+Disconnect from the voice channel
 ```
 ?leave
 ```
--- cogs/music.py
+++ cogs/music.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import youtube_dl
+from requests import get
 
 FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
@@ -9,50 +10,75 @@ FFMPEG_OPTIONS = {
 YDL_OPTIONS = {'format': "bestaudio"}
 
 
+def ytb_search(arg):
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        try:
+            get(arg)
+        except:
+            info = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
+        else:
+            info = ydl.extract_info(arg, download=False)
+
+    return {
+        "source": info["formats"][0]["url"],
+        "title": info["title"],
+        "duration": info["duration"],
+    }
+
+
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.is_playing = False
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("Music Bot is online")
 
-    @commands.command(name='leave')
-    async def disconnect(self, ctx):
-        await ctx.voice_client.disconnect()
-
-    @commands.command(name='join')
+    @commands.command(name='join', help="Connect to a voice channel")
     async def join(self, ctx):
         if ctx.author.voice is None:
-            print('asdsada')
-            await ctx.send("You are not in a voice channel!")
+            await ctx.send("Please connect to a voice channel!")
         else:
             voice_channel = ctx.author.voice.channel
             if ctx.voice_client is None:
                 await voice_channel.connect()
+                print("Joined: ", voice_channel)
             else:
                 await ctx.voice_client.move_to(voice_channel)
+                print("Joined: ", voice_channel)
 
-    @commands.command(name='play')
-    async def play(self, ctx, url):
-        ctx.voice_client.stop()
-        vc = ctx.voice_client
+    @commands.command(name='play', help="Play s youtube_url or youtube query")
+    async def play(self, ctx, *args):
+        if ctx.voice_client is not None:
+            ctx.voice_client.stop()
+            vc = ctx.voice_client
 
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            query = " ".join(args)
+            song = ytb_search(query)
+
+            await ctx.send(f""":headphones: **{song["title"]}** has been added to the queue by {ctx.author.mention}""")
+
+            source = await discord.FFmpegOpusAudio.from_probe(song["source"], **FFMPEG_OPTIONS)
             vc.play(source)
 
-    @commands.command(play='pause')
-    async def pause(self, ctx):
-        ctx.voice_client.pause()
-        await ctx.send("Paused")
+    @commands.command(name='leave', help="Disconnect from the voice channel")
+    async def leave(self, ctx):
+        if ctx.voice_client is not None:
+            await ctx.voice_client.disconnect()
+            print("Disconnected")
 
-    @commands.command(play='resume')
+    @commands.command(play='pause', help="Pause playing song")
+    async def pause(self, ctx):
+        if ctx.voice_client is not None:
+            ctx.voice_client.pause()
+            await ctx.send(f""":headphones: Paused""")
+
+    @commands.command(play='resume', help="Resume playing song")
     async def resume(self, ctx):
-        ctx.voice_client.resume()
-        await ctx.send("Resumed")
+        if ctx.voice_client is not None:
+            ctx.voice_client.resume()
+            await ctx.send(f""":headphones: Resumed""")
 
 
 def setup(bot):
